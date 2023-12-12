cd23805a06dad36e3e79d1a4d87c13541d220cbc
Ben Jefferies <benjefferies@echosoft.uk>
2023-12-12 11:07:14 +0000

    refactor(cli): change repo input format to 'organisation/repo' for better usability

--- src/index.ts
+++ src/index.ts
@@ -24,11 +24,12 @@ program
 
   Example:
   export GITHUB_TOKEN=...
-  wait-for-github-workflow trigger -o VEV-platform-services -r vev-infrastructure -w "Lint" -b abcdef
+  wait-for-github-workflow trigger -r VEV-platform-services/vev-service -w "Lint" -b abcdef
   `,
   )
-  .addOption(new Option("-o, --owner <owner>", "The owner of the repository").default(process.env.GITHUB_ACTOR).makeOptionMandatory(true))
-  .addOption(new Option("-r, --repo <repo>", "The repository name").default(process.env.GITHUB_REPOSITORY).makeOptionMandatory(true))
+  .addOption(
+    new Option("-r, --repo <repo>", "The repository name in the format 'organisation/repo'").default(process.env.GITHUB_REPOSITORY).makeOptionMandatory(true),
+  )
   .addOption(new Option("-w, --workflow_name <workflow_name>", "The workflow name").default(process.env.WORKFLOW_NAME).makeOptionMandatory(true))
   .addOption(new Option("-b, --branch <branch>", "The branch the workflow will triggered on").default(process.env.GITHUB_BRANCH).makeOptionMandatory(true))
   .addOption(new Option("-t, --github_token <github_token>", "The github token").default(process.env.GITHUB_TOKEN).makeOptionMandatory(true))
@@ -46,11 +47,12 @@ program
 
   Example:
   export GITHUB_TOKEN=...
-  wait-for-github-workflow wait -o VEV-platform-services -r vev-infrastructure -w "Pull request" "Lint"  -b abcdef
+  wait-for-github-workflow wait -r VEV-platform-services/vev-service -w "Pull request" "Lint"  -b abcdef
   `,
   )
-  .addOption(new Option("-o, --owner <owner>", "The owner of the repository").default(process.env.GITHUB_ACTOR).makeOptionMandatory(true))
-  .addOption(new Option("-r, --repo <repo>", "The repository name").default(process.env.GITHUB_REPOSITORY).makeOptionMandatory(true))
+  .addOption(
+    new Option("-r, --repo <repo>", "The repository name in the format 'organisation/repo'").default(process.env.GITHUB_REPOSITORY).makeOptionMandatory(true),
+  )
   .addOption(new Option("-w, --workflow_name <items...>", "The workflow name").default(process.env.WORKFLOW_NAME).makeOptionMandatory(true))
   .addOption(new Option("-b, --ref <ref>", "The commit sha the workflow was triggered on").default(process.env.GITHUB_SHA).makeOptionMandatory(true))
   .addOption(new Option("-t, --github_token <github_token>", "The github token").default(process.env.GITHUB_TOKEN).makeOptionMandatory(true))
